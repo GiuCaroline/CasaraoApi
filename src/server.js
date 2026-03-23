87b6b7ae2b5b299bd46d04088fbdb48328a61924
Giulia Caroline <150097558+GiuCaroline@users.noreply.github.com>
2026-03-23 14:47:16 +0000

    Fix: Começa alterações no login.

--- src/server.js
+++ src/server.js
@@ -151,13 +151,11 @@ app.post('/auth/register', async (req, res) => {
     }
 });
 
-
-
 app.post('/auth/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        const user = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
 
         if (user.rows.length === 0) {
             return res.status(400).json({ error: 'Credenciais inválidas' });
